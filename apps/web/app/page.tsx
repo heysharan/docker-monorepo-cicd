e99e17d6299178a6fb0869b7c0b5c552046d2a07
Sharan C M <cmsharan.2019@gmail.com>
2025-08-24 14:21:29 +0530

    changed styles in web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,8 +3,9 @@ import { prismaClient } from "db/client";
 export default async function Page() {
   const users = await prismaClient.user.findMany()
   return (
-    <div className="text-2xl font-semibold">
-      {JSON.stringify(users)}
+    <div className="text-2xl font-semibold flex flex-col gap-2 ml-4 mr-4 mt-4">
+      {users.map((usr,index) => <Button key={index} title={usr.firstname}/>)}
+      {users.map((usr,index) => <Button key={index} title={usr.username}/>)}
     </div>
   );
 }
@@ -12,3 +13,11 @@ export default async function Page() {
 // export const revalidate = 60
 
 export const dynamic = "force-dynamic";
+
+const Button = ({ title } : { title: string }) => {
+  return(
+    <div className="bg-neutral-600 p-3 rounded-lg w-fit ">
+      {title}
+    </div>
+  )
+}
